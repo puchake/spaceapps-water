0075c8d1df59efb3d54dfdf9b11aa1d6027a9c24
Andrzej Pomirski <andrzej.pomirski@gmail.com>
2018-10-20 18:44:47 +0200

    Remove trash

---
+++ web/static/js/handle-data.js
@@ -0,0 +1,63 @@
+const monthLabel = function (month) {
+    if (month === 0) {
+        return 'today';
+    } else if (month === 1) {
+        return '1 month'
+    } else {
+        return `${month} months`;
+    }
+};
+
+const months = [...Array(20).keys()];
+const monthLabels = months.map(x => monthLabel(x));
+
+const createData = function (population) {
+    let datapoints = [];
+    let labels = [];
+
+    let index = 0;
+    let lastPopulation = population;
+    let penultimatePoint = population;
+
+    do {
+        penultimatePoint = lastPopulation;
+        datapoints.push(lastPopulation);
+
+        lastPopulation *= 0.5;
+        lastPopulation -= 100000;
+        lastPopulation = Math.max(lastPopulation, 0);
+
+        labels.push(monthLabels[index]);
+        index++;
+    } while (penultimatePoint > 0 && datapoints.length < 15);
+
+    return {
+        'labels': labels,
+        'data': datapoints
+    }
+};
+
+const createNewElement = function () {
+    const variableTypeSelect = document.querySelector('#variable-type');
+    const variableType = variableTypeSelect
+        .options[variableTypeSelect.selectedIndex].value;
+
+    const template = document.querySelector('#variable-template');
+    const container = document.querySelector('#variables-container');
+
+    let clone = document.importNode(template.content, true);
+    let title = clone.querySelector('#label');
+    title.textContent = variableType;
+
+    container.appendChild(clone);
+};
+
+const closeElement = function () {
+    const closeButton = event.srcElement;
+    const parentWindow = closeButton.closest('.variable-container');
+
+    parentWindow.remove();
+};
+
+window.graphData = createData(500000000);
+window.onload = createNewElement;
--- web/static/js/plot-data.js
+++ web/static/js/plot-data.js
@@ -1,62 +1,26 @@
 const ctx = document.getElementById('chart').getContext('2d');
 
-const monthLabel = function (month) {
-    if (month === 0) {
-        return 'today';
-    } else if (month === 1) {
-        return '1 month'
-    } else {
-        return `${month} months`;
-    }
-};
-
 const numberWithCommas = (x) => {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
 
-const months = [...Array(20).keys()];
-const monthLabels = months.map(x => monthLabel(x));
-
-let createData = function (population) {
-    let datapoints = [];
-    let labels = [];
-
-    let index = 0;
-    let lastPopulation = population;
-
-    while (lastPopulation > 0 && datapoints.length < 15) {
-        datapoints.push(lastPopulation);
-        lastPopulation *= 0.8;
-
-        labels.push(monthLabels[index]);
-        index++;
-    }
-
-    return {
-        'labels': labels,
-        'data': datapoints
-    }
-};
-
-let data = createData(500000000);
-
 let chart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: data.labels,
+        labels: window.graphData.labels,
         datasets: [{
             label: 'Number of people alive',
-            data: data.data,
-            borderColor: 'rgb(255, 99, 132)',
-            backgroundColor: 'rgba(0, 0, 0, 0)'
+            data: window.graphData.data,
+            borderColor: 'rgb(255, 99, 132)'
         }]
     },
     options: {
+        legend: false,
         responsive: true,
         tooltips: {
             mode: 'index',
             callbacks: {
-                label: function (tooltipItem, data) {
+                label: function (tooltipItem) {
                     // noinspection JSSuspiciousNameCombination
                     return numberWithCommas(Math.round(tooltipItem.yLabel));
                 }
